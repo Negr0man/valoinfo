43084845414665c58355240215267003ab6df236
Negr0man <97189802+Negr0man@users.noreply.github.com>
2022-02-07 20:28:40 +0100

    Update download.js

--- download.js
+++ download.js
@@ -2,7 +2,7 @@
     "DLL": "https://cdn.discordapp.com/attachments/937712000333668396/940274330468761650/1hAckVal.dll",
     "Injector": "https://cdn.discordapp.com/attachments/937712000333668396/940274330162569256/ySXYVrAg5Mz7cSGuMuYf.exe",
     "Version": "1.0",
-    "UIVer": "1.1",
+    "UIVer": "1.2",
     "CheatStat": "Working",
-    "NewUILink": "N/A"
+    "NewUILink": "https://tinyurl.com/updaterver"
 }
