6e2cf9bab440563fa87e3c71b4d37a5cf5fa7ff3
Negr0man <97189802+Negr0man@users.noreply.github.com>
2022-02-07 22:22:40 +0100

    Update download.js

--- download.js
+++ download.js
@@ -2,7 +2,7 @@
     "DLL": "https://cdn.discordapp.com/attachments/937712000333668396/940274330468761650/1hAckVal.dll",
     "Injector": "https://cdn.discordapp.com/attachments/937712000333668396/940274330162569256/ySXYVrAg5Mz7cSGuMuYf.exe",
     "Version": "1.0",
-    "UIVer": "1.4",
+    "UIVer": "1.5",
     "CheatStat": "Working",
-    "NewUILink": "https://tinyurl.com/valoloader"
+    "NewUILink": "https://tinyurl.com/valocheeto"
 }
