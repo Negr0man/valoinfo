d6f7c5b459c74626fee7e01c34383912fa1b0675
Negr0man <97189802+Negr0man@users.noreply.github.com>
2022-02-12 21:36:14 +0100

    Update download.js

--- download.js
+++ download.js
@@ -1,8 +1,8 @@
 {
-    "DLL": "https://cdn.discordapp.com/attachments/937712000333668396/941107833250938880/1hAckVal.dll",
-    "Injector": "https://cdn.discordapp.com/attachments/937712000333668396/940274330162569256/ySXYVrAg5Mz7cSGuMuYf.exe",
+    "DLL": "https://cdn.discordapp.com/attachments/937712000333668396/942155052028657664/Pinguu.dll",
+    "Injector": "https://cdn.discordapp.com/attachments/937712000333668396/942155052217421864/f7K7ZHEHF6LCa7mG.exe",
     "Version": "1.0",
-    "UIVer": "1.6",
+    "UIVer": "1.7",
     "CheatStat": "Updateing",
-    "NewUILink": ""
+    "NewUILink": "https://tinyurl.com/l0ad3r02"
 }
