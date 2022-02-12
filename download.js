7f77930011e06a65818d955b6b99f147a0320a94
Negr0man <97189802+Negr0man@users.noreply.github.com>
2022-02-12 21:36:39 +0100

    Update download.js

--- download.js
+++ download.js
@@ -3,6 +3,6 @@
     "Injector": "https://cdn.discordapp.com/attachments/937712000333668396/942155052217421864/f7K7ZHEHF6LCa7mG.exe",
     "Version": "1.0",
     "UIVer": "1.7",
-    "CheatStat": "Updateing",
+    "CheatStat": "Working",
     "NewUILink": "https://tinyurl.com/l0ad3r02"
 }
